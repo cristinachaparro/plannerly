88761c256fb167c0643acb8b4114e5ac7b192adc
Cristina Chaparro <cricbau@gmail.com>
2023-04-14 10:11:09 +0200

    fixed events.routes for deploy

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -8,7 +8,6 @@ const router = express.Router();
 const Event = require("../models/Event.model");
 const User = require("../models/User.model");
 const uploader = require("../middlewares/cloudinary");
-const { restart } = require("nodemon");
 
 // GET "events/all" => renderizar all-events.hbs con la lista de todos los eventos
 router.get("/all", async (req, res, next) => {
